4c9423209a53c6836066341f7bf7738f733d4662
Joe-Andrews <240369800@aston.ac.uk>
2024-11-04 13:15:26 +0000

    Add files via upload

--- ProjectStarterCode_MySQL/src/model/Product.java
+++ ProjectStarterCode_MySQL/src/model/Product.java
@@ -3,77 +3,77 @@ package model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class Product {
-	private int id; // id=0 means no id set (a new, unsaved, product)
-	private String name;
-	private String description;
-	private int price;
+public abstract class Product {
+	protected int id; // id=0 means no id set (a new, unsaved, product)
+	protected String name;
+	protected String supplier;
+	protected int price;
 	
-	public Product(String name, String description, int price){
-		this(0, name, description, price);
+	//there are different constructors for if the id is different
+	protected Product(String name, String supplier, int price){
+		this(0, name, supplier, price);
 	}
 	
-	public Product(int id, String name, String description, int price){
+	protected Product(int id, String name, String supplier, int price){
 		this.id = id;
 		this.name = name;
-		this.description = description;
+		this.supplier = supplier;
 		this.price = price;
 	}
 	
-
 	/**
 	 * @return the id
 	 */
-	public int getId() {
+	protected int getId() {
 		return id;
 	}
 
 	/**
 	 * @param id the id to set
 	 */
-	public void setId(int id) {
+	protected void setId(int id) {
 		this.id = id;
 	}
 
 	/**
 	 * @return the name
 	 */
-	public String getName() {
+	protected String getName() {
 		return name;
 	}
 
 	/**
 	 * @param name the name to set
 	 */
-	public void setName(String name) {
+	protected void setName(String name) {
 		this.name = name;
 	}
 
 	/**
 	 * @return the description
 	 */
-	public String getDescription() {
-		return description;
+	protected String getSupplier() {
+		return supplier;
 	}
 
 	/**
 	 * @param description the description to set
 	 */
-	public void setDescription(String description) {
-		this.description = description;
+	protected void setSupplier(String supplier) {
+		this.supplier = supplier;
 	}
 
 	/**
 	 * @return the price
 	 */
-	public int getPrice() {
+	protected int getPrice() {
 		return price;
 	}
 
 	/**
 	 * @param price the price to set
 	 */
-	public void setPrice(int price) {
+	protected void setPrice(int price) {
 		this.price = price;
 	}
 	
@@ -81,14 +81,17 @@ public class Product {
 	 * 
 	 * @return price formatted with two decimal places to represent pounds & pence (or dollars & cents, or...)
 	 */
-	public double getFormattedPrice(){
+	protected double getFormattedPrice(){
 		BigDecimal bd = new BigDecimal(price / 100.0);
 	    bd = bd.setScale(2, RoundingMode.HALF_UP);
 	    return bd.doubleValue();
 	}
 	
+	/**
+	 * base description of any item
+	 */
 	public String toString(){
-		return name + " " + getFormattedPrice() + "\n" + description;
+		return name + " " + getFormattedPrice() + "\n From " + supplier + "\n";
 	}
 
 }
